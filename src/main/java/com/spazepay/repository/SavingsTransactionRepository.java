042172903420a88c6a0bb9ebdfe3e5b5b1dc795b
Ndifreke Eduo <alexanderndifreke4@gmail.com>
2025-04-08 15:05:40 +0100

    Implemented unit tests

--- src/main/java/com/spazepay/repository/SavingsTransactionRepository.java
+++ src/main/java/com/spazepay/repository/SavingsTransactionRepository.java
@@ -4,6 +4,8 @@ import com.spazepay.model.SavingsPlan;
 import com.spazepay.model.SavingsTransaction;
 import com.spazepay.model.enums.TransactionType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.Instant;
 import java.util.List;
@@ -14,4 +16,13 @@ public interface SavingsTransactionRepository extends JpaRepository<SavingsTrans
     List<SavingsTransaction> findByPlan(SavingsPlan plan);
 
     List<SavingsTransaction> findByPlanAndType(SavingsPlan plan, TransactionType transactionType);
+
+    @Query("SELECT t FROM SavingsTransaction t " +
+            "WHERE t.plan = :plan " +
+            "AND t.type = :type " +
+            "AND TO_CHAR(t.timestamp, 'YYYY-MM') = :month")
+    List<SavingsTransaction> findByPlanAndTypeAndMonth(
+            @Param("plan") SavingsPlan plan,
+            @Param("type") TransactionType type,
+            @Param("month") String month);
 }
